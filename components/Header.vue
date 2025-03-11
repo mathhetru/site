997d76eed27a82e2e66113ff169c57f830620527
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2025-03-11 09:43:13 +0100

    feat(front): add primeVue (#1)

--- components/Header.vue
+++ components/Header.vue
@@ -12,39 +12,40 @@
         <p class="line__text">portfolio</p>
         <div class="line__change"></div>
       </NuxtLink>
-      <!-- <NuxtLink class="line" to="/skills">
-        <p class="line__text">compétences</p>
-        <div class="line__change"></div>
-      </NuxtLink> -->
-      <div @click="handleClickCollapsible" class="line">
+      <Button
+        @click="toggleServices"
+        aria-haspopup="true"
+        aria-controls="overlay_menu"
+        :aria-expanded="isMenuOpen"
+        class="line"
+        type="button"
+      >
         <p class="line__text">
           services
-          <font-awesome-icon
-            class="ml-2 relative"
-            :icon="arrowDirection()"
-          />
+          <font-awesome-icon class="ml-2 relative" :icon="arrowDirection" />
         </p>
         <div class="line__change"></div>
-        <div v-show="openModalLinks">
-          <div class="nav-collapsible">
-            <NuxtLink class="nav-collapsible__link" to="/services-dev-web">
-              <p class="nav-collapsible__text">developpement web</p>
-              <div class="nav-collapsible__change"></div>
-            </NuxtLink>
-            <NuxtLink
-              class="nav-collapsible__link"
-              to="/services-graphic-design"
-            >
-              <p class="nav-collapsible__text">graphisme print</p>
-              <div class="nav-collapsible__change"></div>
-            </NuxtLink>
-            <NuxtLink class="nav-collapsible__link" to="/services-illustration">
-              <p class="nav-collapsible__text">illustration</p>
-              <div class="nav-collapsible__change"></div>
-            </NuxtLink>
-          </div>
-        </div>
-      </div>
+      </Button>
+      <Menu
+        ref="menuRef"
+        id="overlay_menu"
+        :model="items"
+        class="nav-collapsible"
+        :popup="true"
+        @hide="menuClose()"
+        @show="menuOpen()"
+      >
+        <template #item="{ item }">
+          <NuxtLink
+            v-if="item.route"
+            :to="item.route"
+            class="nav-collapsible__link"
+          >
+            <p class="nav-collapsible__text">{{ item.label }}</p>
+            <div class="nav-collapsible__change"></div>
+          </NuxtLink>
+        </template>
+      </Menu>
       <NuxtLink class="line" to="/about">
         <p class="line__text">qui suis-je ?</p>
         <div class="line__change"></div>
@@ -66,20 +67,35 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { ref, computed } from "vue";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import Menu from "primevue/menu";
+import Button from "primevue/button";
 
-const openModalLinks = ref(false);
+const menuRef = ref();
+const isMenuOpen = ref(false);
 
-const arrowDirection = () => {
-  return openModalLinks.value ? fas.faAngleUp : fas.faAngleDown
+const items = ref([
+  { label: "Développement web", route: "/services-dev-web" },
+  { label: "Graphisme print et digital", route: "/services-graphic-design" },
+  { label: "Illustration", route: "/services-illustration" },
+]);
+
+const arrowDirection = computed(() =>
+  isMenuOpen.value ? faAngleUp : faAngleDown,
+);
+
+const toggleServices = (event: Event) => {
+  if (menuRef.value) {
+    menuRef.value.toggle(event);
+  }
 };
 
-const handleClickCollapsible = () => {
-  openModalLinks.value = !openModalLinks.value;
+const menuOpen = () => {
+  isMenuOpen.value = true;
 };
 
-const closeModalLinks = () => {
-  openModalLinks.value = false;
+const menuClose = () => {
+  isMenuOpen.value = false;
 };
 </script>
---
+++ pages/Services-Dev-Web.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <p>À propos de nous</p>
+  </div>
+</template>
